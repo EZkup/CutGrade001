9178c0497908dbb66c172934b37d7806ebe0e9ec
Teacher <Denrip2018@gmail.com>
2024-05-02 14:40:01 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -4,12 +4,41 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("In put you score");
         Scanner scanner = new Scanner(System.in);
-        int scannner = scanner.nextInt();
-        if (scannner >= 50) {
-        System.out.println("1");
-
-
+        int sc = scanner.nextInt();
+        if (sc >= 80 ) {
+        System.out.println("Grade 4 ");
+        System.out.println("Score "+sc);
         }
+        else if (sc >= 75 ) {
+            System.out.println("Grade 3.5 ");
+            System.out.println("Score "+sc);
+        }
+        else if (sc >= 70 ) {
+            System.out.println("Grade 3 ");
+            System.out.println("Score "+sc);
+        }
+        else if (sc >= 65 ) {
+            System.out.println("Grade 2.5 ");
+            System.out.println("Score "+sc);
+        }
+        else if (sc >= 60 ) {
+            System.out.println("Grade 2 ");
+            System.out.println("Score "+sc);
+        }
+        else if (sc >= 55 ) {
+            System.out.println("Grade 1.5 ");
+            System.out.println("Score "+sc);
+        }
+        else if (sc >=  50 ) {
+            System.out.println("Grade 1");
+            System.out.println("Score "+sc);
+        }
+        else if (sc >= 49) {
+            System.out.println("Grade 0");
+            System.out.println("Score "+sc);
+        }
+
     }
 }
